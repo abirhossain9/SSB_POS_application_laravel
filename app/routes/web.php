ddcf137518d02e6f4b28a997f3b70456fa3026c2
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-01 17:57:05 +0600

    Route updated #7

--- app/routes/web.php
+++ app/routes/web.php
@@ -19,6 +19,15 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'],function(){
     route::get('/dashboard','App\Http\Controllers\Backend\DashboardController@dashboard')->name('dashboard');
+     //this routes are for employee management
+     Route::group(['prefix' => '/employee'], function(){
+        Route::get('/manage','App\Http\Controllers\Backend\EmployeesController@index')->name('employee.manage');
+        Route::get('/create','App\Http\Controllers\Backend\EmployeesController@create')->name('employee.create');
+        Route::post('/store','App\Http\Controllers\Backend\EmployeesController@store')->name('employee.store');
+        Route::get('/edit/{id}','App\Http\Controllers\Backend\EmployeesController@edit')->name('employee.edit');
+        Route::post('/update/{id}','App\Http\Controllers\Backend\EmployeesController@update')->name('employee.update');
+        Route::post('/destroy/{id}','App\Http\Controllers\Backend\EmployeesController@destroy')->name('employee.destroy');
+    });
 });
 
 route::get('/blank-page','App\Http\Controllers\Backend\DashboardController@blank')->name('blank');

b94df3ad27b7c4b85918bdb35200c8ec2e70131c
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-01 17:56:30 +0600

    model updated #7

--- app/app/Models/Backend/Employees.php
+++ app/app/Models/Backend/Employees.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Employees extends Model
 {
     use HasFactory;
+    protected $fillbale = [
+        'fullname',
+        'gender',
+        'company',
+        'slug',
+        'phone',
+        'address',
+        'email',
+        'image',
+        'group',
+        'status',
+    ];
 }
--- app/database/migrations/2021_10_28_094548_create_employees_table.php
+++ app/database/migrations/2021_10_28_094548_create_employees_table.php
@@ -15,6 +15,16 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('fullname');
+            $table->string('gender')->default(3)->comment('1 = Male, 2 = Female, 3 = undefined')->nullable();
+            $table->string('company');
+            $table->string('slug');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->text('image')->nullable();
+            $table->string('group')->default(3)->comment('1= Owner, 2 = Admin, 3 = Sales');
+            $table->string('status')->default(2)->comment('1 = Active, 2 = Inactive');
             $table->timestamps();
         });
     }
